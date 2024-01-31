d858421136510af58669be406d4d0ff1d1216b5e
Oleksandr Hrabovets <alex6754035@gmail.com>
2024-01-31 08:28:00 +0200

    add ReplyKeyboardFactory

---
+++ src/main/java/ua/oh/telegram_bot/bot/ReplyKeyboardFactory.java
@@ -0,0 +1,26 @@
+package ua.oh.telegram_bot.bot;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+@Component
+public class ReplyKeyboardFactory {
+
+  public ReplyKeyboardMarkup getReplyKeyboard(KeyboardRow row) {
+
+    ReplyKeyboardMarkup replyKeyboard = new ReplyKeyboardMarkup();
+    replyKeyboard.setResizeKeyboard(true);
+    replyKeyboard.setSelective(false);
+
+    List<KeyboardRow> keyboardRows = new ArrayList<>();
+
+    keyboardRows.add(row);
+    replyKeyboard.setKeyboard(keyboardRows);
+    return replyKeyboard;
+
+  }
+
+}
